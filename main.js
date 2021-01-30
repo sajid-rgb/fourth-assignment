14b0d9313889f9c352db3d5f48512cc843a49f6b
Sajid Chowdhury <msrchk@gmail.com>
2021-01-30 20:21:28 +0600

    last push

--- main.js
+++ main.js
@@ -1,37 +1,22 @@
-const economyIncrease = document.getElementById("economyIncrease");
-const economyDecrease = document.getElementById("economyDecrease");
-const firstClassIncrease = document.getElementById("firstClassIncrease");
-const firstClassDecrease = document.getElementById("firstClassDecrease");
-economyIncrease.addEventListener("click", function () {
-    ticketNumber(true, "economy")
-})
-economyDecrease.addEventListener("click", function () {
-    ticketNumber(false, 'economy')
-})
-firstClassIncrease.addEventListener("click", function () {
-    ticketNumber(true, "firstClass")
-})
-firstClassDecrease.addEventListener("click", function () {
-    ticketNumber(false, "firstClass")
-})
+//This function used for get ticket input and increased it when press plus minus button
 function ticketNumber(isIncrease, ticket) {
-    let ticketInput = document.getElementById(ticket + "Input").value;
+    let ticketInput = getInput(ticket+"Input");
     if (isIncrease == true) {
         ticketInput++;
         if (ticket == "firstClass" && ticketInput >= 0) {
-            bookingInformation("firstClass", ticketInput)
+            bookingInformation("firstClass", ticketInput);
         }
         if (ticket == "economy" && ticketInput >= 0) {
-            bookingInformation("economy", ticketInput)
+            bookingInformation("economy", ticketInput);
         }
     }
     if (isIncrease == false) {
         ticketInput--;
         if (ticket == "firstClass" && ticketInput >= 0) {
-            bookingInformation("firstClass", ticketInput)
+            bookingInformation("firstClass", ticketInput);
         }
         if (ticket == "economy" && ticketInput >= 0) {
-            bookingInformation("economy", ticketInput)
+            bookingInformation("economy", ticketInput);
         }
     }
     if (ticketInput >= 0) {
@@ -39,17 +24,47 @@ function ticketNumber(isIncrease, ticket) {
     }
     fareCalculation();
 }
+//This function used for calculate sub total,total and tax.
 function fareCalculation() {
-    const firstClassInput = document.getElementById("firstClassInput").value;
-    const economyInput = document.getElementById("economyInput").value;
-    const subTotalFare = firstClassInput * 150 + economyInput * 100;
-    const tax = subTotalFare * .1;
-    const totalFare = subTotalFare + tax;
-    document.getElementById("sub-total").innerText = "$" + subTotalFare
-    document.getElementById("tax").innerText = "$" + tax
-    document.getElementById("total").innerText = "$" + totalFare
-    document.getElementById("payTotal").innerText = "$" + totalFare
+    const firstClassInput = getInput("firstClassInput");
+    const economyInput = getInput("economyInput");
+    if (firstClassInput > 0 || economyInput) {
+        const subTotalFare = firstClassInput * 150 + economyInput * 100;
+        const tax = subTotalFare * .1;
+        const totalFare = subTotalFare + tax;
+        updateCalculation("sub-total", subTotalFare);
+        updateCalculation("vat-total", tax);
+        updateCalculation("grand-total", totalFare);
+        updateCalculation("pay-total", totalFare);
+        updateCalculation("purchase-total", totalFare);
+    }
+    else if (firstClassInput <= 0 || economyInput <= 0) {
+        updateCalculation("sub-total", 0);
+        updateCalculation("vat-total", 0);
+        updateCalculation("grand-total", 0);
+        updateCalculation("pay-total", 0);
+        updateCalculation("purchase-total", 0);
+    }
 }
+//This function used for show the sub total,tax and total in their section
+function updateCalculation(id, total) {
+    if (id == "sub-total") {
+        document.getElementById(id).innerText = "$" + total;
+    }
+    if (id == "vat-total") {
+        document.getElementById(id).innerText = "$" + total;
+    }
+    if (id == "grand-total") {
+        document.getElementById(id).innerText = "$" + total;
+    }
+    if (id == "pay-total") {
+        document.getElementById(id).innerText = "$" + total;
+    }
+    if (id == "purchase-total") {
+        document.getElementById(id).innerText = total;
+    }
+}
+//This function used for show the ticket number in confirmation message
 function bookingInformation(myBooking, ticket) {
     if (myBooking == "firstClass") {
         document.getElementById(myBooking + 'Booked').innerText = ticket;
@@ -58,45 +73,48 @@ function bookingInformation(myBooking, ticket) {
         document.getElementById(myBooking + 'Booked').innerText = ticket;
     }
 }
-const confirmation = document.getElementById("confirmation")
-let count = 0;
-confirmation.addEventListener("click", function () {
-    count++;
-    if (count == 1) {
-        document.getElementById('accountNumber').style.display = "none";
-        document.getElementById('verification').style.display = "block";
-        document.getElementById('verification').style.position = "relative";
-        document.getElementById('verification').style.bottom = "5px";
+//This function used for verify account for transaction
+function verifyAccount(id, value) {
+    if (id == "accountNumber" && value == 1) {
+        document.getElementById(id).style.display = "none";
+        document.getElementById('confirmation').style.display = "none";
     }
-    else {
-        document.getElementById('verification').style.display = "none";
-        document.getElementById('password').style.display = "block";
-        document.getElementById('password').style.position = "relative";
-        document.getElementById('password').style.bottom = "30px";
+    if ((id == "verification" && value == 2) || (id == "password" && value == 3)) {
+        document.getElementById('confirmation2').style.display = "block";
+        document.getElementById(id).style.display = "block";
     }
-})
-const bookNow = document.getElementById("bookNow")
-bookNow.addEventListener("click", function () {
-    const ticketInputNumber = document.getElementById("firstClassInput").value;
-    const ticketInputNumber2 = document.getElementById("economyInput").value;
-    if (ticketInputNumber == "" && ticketInputNumber2 == "") {
-        alert("Please select your sit.")
+}
+//This function used for visible congratulate section when confirm button click
+function confirmButtonClick() {
+    document.getElementById("accountSection").style.display = "none";
+    document.getElementById("congratulateSection").style.display = "block";    
+}
+//This button used for confirmation section
+function bookingConfirmation() {
+    const firstClassInputNumber =getInput("firstClassInput");
+    const economyInputNumber = getInput("economyInput");
+    if ((firstClassInputNumber == "" && economyInputNumber == "") || (firstClassInputNumber <= 0 && economyInputNumber <= 0)) {
+        alert("Please select your sit.");
     }
     else {
         document.getElementById("bookingSection").style.display = "none";
         document.querySelector("#confirmation-section").style.display = "block";
     }
-})
-const yes = document.getElementById("yes")
-yes.addEventListener("click", function () {
+}
+//This function used for click yes button and visible account section
+function yesButtonClick() {
     document.querySelector("#confirmation-section").style.display = "none";
     document.querySelector(".account-section").style.display = "block";
     document.querySelector("#no").style.display = "block";
-})
-const no = document.getElementById("no")
-no.addEventListener("click", function () {
+}
+//This function used for click no button and visible booking section and hide confirmation section
+function noButtonClick() {
     document.querySelector("#bookingSection").style.display = "block";
     document.querySelector("#confirmation-section").style.display = "none";
-})
-
+}
+//This function used for get input value
+function getInput(id){
+    const ticketId = document.getElementById(id).value;
+    return ticketId;
+}
 
